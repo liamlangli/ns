38db71a8a34db3fde95f793c843460afb26ad5c6
lilang.liam <lilang.liam@bytedance.com>
2024-01-09 20:59:42 +0800

    fix rebase error

--- bison/ns_lexer.tab.h
+++ bison/ns_lexer.tab.h
@@ -143,12 +143,8 @@ extern int yydebug;
 
 /* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-<<<<<<< Updated upstream
-union YYSTYPE
-=======
 typedef union YYSTYPE
 #line 10 "ns_lexer.y"
->>>>>>> Stashed changes
 {
 #line 8 "ns_lexer.y"
 
