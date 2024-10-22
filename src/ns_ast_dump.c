49228b9cd801aa7a483ad4c58a58dda3905d1c42
lilang.liam <lilang.liam@bytedance.com>
2024-10-22 18:04:18 +0800

    dye more keyword

--- src/ns_ast_dump.c
+++ src/ns_ast_dump.c
@@ -88,7 +88,7 @@ void ns_ast_dump(ns_ast_ctx *ctx, i32 i) {
         }
 
         if (n.fn_def.body != -1)
-            printf(" { node[%d] }", n.fn_def.body);
+            printf(" { [%d] }", n.fn_def.body);
         else {
             printf(";");
         }
@@ -115,7 +115,7 @@ void ns_ast_dump(ns_ast_ctx *ctx, i32 i) {
             ns_str_printf(n.ops_fn_def.return_type.val);
         }
         if (n.ops_fn_def.body != -1) {
-            printf(" { node[%d] }", n.ops_fn_def.body);
+            printf(" { [%d] }", n.ops_fn_def.body);
         }
     } break;
     case NS_AST_STRUCT_DEF: {
@@ -146,52 +146,53 @@ void ns_ast_dump(ns_ast_ctx *ctx, i32 i) {
         }
         break;
     case NS_AST_VAR_DEF:
+        printf(ns_color_log "let " ns_color_nil);
         ns_str_printf(n.var_def.name.val);
         if (n.var_def.type.type != NS_TOKEN_INVALID) {
             printf(":");
             ns_str_printf(n.var_def.type.val);
         }
         if (n.var_def.expr != -1) {
-            printf(" = node[%d]", n.var_def.expr);
+            printf(" = [%d]", n.var_def.expr);
         }
         break;
     case NS_AST_EXPR:
-        printf("node[%d]", n.expr.body);
+        printf("[%d]", n.expr.body);
         break;
     case NS_AST_CAST_EXPR:
-        printf("node[%d] as ", n.cast_expr.expr);
+        printf("[%d] as ", n.cast_expr.expr);
         ns_str_printf(n.cast_expr.type.val);
         break;
     case NS_AST_PRIMARY_EXPR: {
         if (n.primary_expr.token.type == NS_TOKEN_UNKNOWN) {
-            printf("node[%d]", n.primary_expr.expr);
+            printf("[%d]", n.primary_expr.expr);
         } else {
             ns_str_printf(n.primary_expr.token.val);
         }
     } break;
     case NS_AST_BINARY_EXPR:
-        printf("node[%d] ", n.binary_expr.left);
+        printf("[%d] ", n.binary_expr.left);
         ns_str_printf(n.binary_expr.op.val);
-        printf(" node[%d]", n.binary_expr.right);
+        printf(" [%d]", n.binary_expr.right);
         break;
     case NS_AST_UNARY_EXPR:
         ns_str_printf(n.unary_expr.op.val);
-        printf(" node[%d]", n.unary_expr.expr);
+        printf(" [%d]", n.unary_expr.expr);
         break;
     case NS_AST_FIELD_DEF: {
         ns_str_printf(n.field_def.name.val);
-        printf(" = node[%d]", n.field_def.expr);
+        printf(" = [%d]", n.field_def.expr);
     } break;
     case NS_AST_MEMBER_EXPR: {
-        printf("node[%d].", n.member_expr.left);
+        printf("[%d].", n.member_expr.left);
         ns_str_printf(n.member_expr.right.val);
     } break;
     case NS_AST_CALL_EXPR:
-        printf("node[%d]", n.call_expr.callee);
+        printf("[%d]", n.call_expr.callee);
         printf("(");
         i32 next = n.call_expr.arg;
         for (i32 i = 0; i < n.call_expr.arg_count; i++) {
-            printf("node[%d]", next);
+            printf("[%d]", next);
             next = ctx->nodes[next].next;
             if (i != n.call_expr.arg_count - 1) {
                 printf(", ");
@@ -202,7 +203,7 @@ void ns_ast_dump(ns_ast_ctx *ctx, i32 i) {
     case NS_AST_JUMP_STMT:
         ns_str_printf(n.jump_stmt.label.val);
         if (n.jump_stmt.expr != -1) {
-            printf(" node[%d]", n.jump_stmt.expr);
+            printf(" [%d]", n.jump_stmt.expr);
         }
         break;
     case NS_AST_COMPOUND_STMT: {
@@ -213,7 +214,7 @@ void ns_ast_dump(ns_ast_ctx *ctx, i32 i) {
         }
         ns_ast_t *stmt = &n;
         for (i32 i = 0; i < n.compound_stmt.count; i++) {
-            printf("node[%d]", stmt->next);
+            printf("[%d]", stmt->next);
             stmt = &ctx->nodes[stmt->next];
             if (i != n.compound_stmt.count - 1) {
                 printf(", ");
@@ -222,9 +223,9 @@ void ns_ast_dump(ns_ast_ctx *ctx, i32 i) {
         printf(" }");
     } break;
     case NS_AST_IF_STMT: {
-        printf("if (node[%d]) node[%d]", n.if_stmt.condition, n.if_stmt.body);
+        printf(ns_color_log "if" ns_color_nil " ([%d]) [%d]", n.if_stmt.condition, n.if_stmt.body);
         if (n.if_stmt.else_body != -1) {
-            printf(" else node[%d]", n.if_stmt.else_body);
+            printf(" " ns_color_log "else" ns_color_nil " [%d]", n.if_stmt.else_body);
         }
     } break;
     case NS_AST_DESIGNATED_EXPR: {
@@ -235,7 +236,7 @@ void ns_ast_dump(ns_ast_ctx *ctx, i32 i) {
         for (i32 i = 0; i < count; i++) {
             field = &ctx->nodes[field->next];
             ns_str_printf(field->field_def.name.val);
-            printf(": node[%d]", field->field_def.expr);
+            printf(": [%d]", field->field_def.expr);
             if (i != count - 1) {
                 printf(", ");
             }
@@ -245,9 +246,9 @@ void ns_ast_dump(ns_ast_ctx *ctx, i32 i) {
     case NS_AST_GEN_EXPR: {
         ns_str_printf(n.gen_expr.name.val);
         if (n.gen_expr.range) {
-            printf(" in node[%d] to node[%d]", n.gen_expr.from, n.gen_expr.to);
+            printf(" " ns_color_log "in" ns_color_nil " [%d] "ns_color_log "to" ns_color_nil " [%d]", n.gen_expr.from, n.gen_expr.to);
         } else {
-            printf(" in node[%d]", n.gen_expr.from);
+            printf(" " ns_color_log "in" ns_color_nil " [%d]", n.gen_expr.from);
         }
     } break;
     case NS_AST_FOR_STMT: {
@@ -255,17 +256,17 @@ void ns_ast_dump(ns_ast_ctx *ctx, i32 i) {
         ns_ast_t gen = ctx->nodes[n.for_stmt.generator];
         ns_str_printf(gen.gen_expr.name.val);
         if (gen.gen_expr.range) {
-            printf(" in node[%d] to node[%d]", gen.gen_expr.from, gen.gen_expr.to);
+            printf(" " ns_color_log "in" ns_color_nil " [%d] " ns_color_log "to" ns_color_nil " [%d]", gen.gen_expr.from, gen.gen_expr.to);
         } else {
-            printf(" in node[%d]", gen.gen_expr.from);
+            printf(" " ns_color_log "in" ns_color_nil " [%d]", gen.gen_expr.from);
         }
-        printf(" { node[%d] }", n.for_stmt.body);
+        printf(" { [%d] }", n.for_stmt.body);
     } break;
     case NS_AST_LOOP_STMT: {
         if (n.loop_stmt.do_first) {
-            printf(ns_color_log "do" ns_color_nil " { node[%d] } loop node[%d]", n.loop_stmt.body, n.loop_stmt.condition);
+            printf(ns_color_log "do" ns_color_nil " { [%d] } loop [%d]", n.loop_stmt.body, n.loop_stmt.condition);
         } else {
-            printf(ns_color_log "loop" ns_color_nil " node[%d] { node[%d] }", n.loop_stmt.condition, n.loop_stmt.body);
+            printf(ns_color_log "loop" ns_color_nil " [%d] { [%d] }", n.loop_stmt.condition, n.loop_stmt.body);
         }
     } break;
     case NS_AST_IMPORT_STMT: {
