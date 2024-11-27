7095dd4f011a8afc043ecddded67feda2bf50d34
lilang.liam <lilang.liam@bytedance.com>
2024-11-27 14:23:46 +0800

    success ful launch nsdb

--- nslang/src/ns_db.ts
+++ nslang/src/ns_db.ts
@@ -1,84 +1,76 @@
-import { LoggingDebugSession, StoppedEvent, TerminatedEvent } from "vscode-debugadapter";
-import { DebugProtocol } from "vscode-debugprotocol";
-import { promises as fs } from 'fs';
-import * as Net from 'net';
+process.stdin.on('error', (err) => {
+	console.error('stdin error:', err);
+	process.exit(1);
+});
 
-export class NSDebugger extends LoggingDebugSession {
-    private running = false;
-    sendSen: any;
+let buffer = Buffer.from([]);
 
-    constructor() {
-        super('nsdb');
-    }
+process.stdin.on('readable', () => {
+	while (process.stdin.readable) {
+		const chunk = process.stdin.read();
+		if (chunk == null || chunk.length === 0) {
+			break;
+		}
 
-    protected initializeRequest(response: DebugProtocol.InitializeResponse, args: DebugProtocol.InitializeRequestArguments): void {
-        response.body = response.body || {};
-        response.body.supportsConfigurationDoneRequest = true;
-        this.sendResponse(response);
-    }
-
-    protected launchRequest(response: DebugProtocol.LaunchResponse, args: DebugProtocol.LaunchRequestArguments, request?: DebugProtocol.Request): void {
-        this.running = true;
-        console.log(args);
-        this.sendEvent(new StoppedEvent("breakpoint", 1));
-        this.sendResponse(response);
-    }
-
-    protected disconnectRequest(response: DebugProtocol.DisconnectResponse, args: DebugProtocol.DisconnectArguments, request?: DebugProtocol.Request): void {
-        this.running = false;
-        this.sendEvent(new TerminatedEvent());
-        this.sendResponse(response);
-    }
-
-    protected setBreakPointsRequest(response: DebugProtocol.SetBreakpointsResponse, args: DebugProtocol.SetBreakpointsArguments, request?: DebugProtocol.Request): void {
-        this.sendResponse(response);
-    }
-}
-
-/*
- * debugAdapter.js is the entrypoint of the debug adapter when it runs as a separate process.
- */
-
-/*
- * Since here we run the debug adapter as a separate ("external") process, it has no access to VS Code API.
- * So we can only use node.js API for accessing files.
- */
-const fs_accessor = {
-	isWindows: process.platform === 'win32',
-	readFile(path: string): Promise<Uint8Array> {
-		return fs.readFile(path);
-	},
-	writeFile(path: string, contents: Uint8Array): Promise<void> {
-		return fs.writeFile(path, contents);
+		buffer = Buffer.concat([buffer, chunk]);
 	}
-};
 
-let port = 0;
-const args = process.argv.slice(2);
-args.forEach(function (val, index, array) {
-	const portMatch = /^--server=(\d{4,5})$/.exec(val);
-	if (portMatch) {
-		port = parseInt(portMatch[1], 10);
+	while (true) {
+		const message = extract_message(buffer);
+		if (message === null) {
+			break;
+		}
+
+		process_message(message);
 	}
 });
 
-if (port > 0) {
-	// start a server that creates a new session for every connection request
-	console.error(`waiting for debug protocol on port ${port}`);
-	Net.createServer((socket) => {
-		console.error('>> accepted connection from client');
-		socket.on('end', () => {
-			console.error('>> client connection closed\n');
-		});
-		const session = new NSDebugger();
-		session.setRunAsServer(true);
-		session.start(socket, socket);
-	}).listen(port);
-} else {
-	// start a single session that communicates via stdin/stdout
-	const session = new NSDebugger();
-	process.on('SIGTERM', () => {
-		session.shutdown();
-	});
-	session.start(process.stdin, process.stdout);
+
+function find_index(buf: Buffer, pat: string): number | null {
+	const pattern: Uint8Array = new TextEncoder().encode(pat)
+
+	for (let i = 0; i + pattern.length <= buffer.length; i++) {
+	  const part = buffer.slice(i, i + pattern.length)
+	  if (part.compare(pattern) === 0) {
+		return i
+	  }
+	}
+	return null
 }
+
+function extract_message(buffer: Buffer): any | null {
+	const header_index = find_index(buffer, '\r\n\r\n');
+	if (header_index == null) {
+		return null;
+	}
+
+	const body_index = header_index + 4;
+	const header = new TextDecoder().decode(buffer.slice(0, header_index));
+	let len: number | null = null;
+	for (const line of header.split('\r\n')) {
+		const [key, value] = line.split(': ');
+		if (key === 'Content-Length') {
+			len = parseInt(value);
+			break;
+		}
+
+		if (key !== '') {
+			console.error('unknown header:', key);
+		}
+	}
+
+	if (len === null) {
+		console.error('Content-Length not found');
+		return null;
+	}
+
+	const body = buffer.slice(body_index, body_index + len);
+	const body_obj = JSON.parse(new TextDecoder().decode(body));
+	return body_obj;
+}
+
+function process_message(request: any) {
+	console.log(request);
+}
+
+console.log('ns_db started');
--- nslang/src/ns_ext.ts
+++ nslang/src/ns_ext.ts
@@ -5,12 +5,12 @@ import {
   ServerOptions,
   TransportKind,
 } from "vscode-languageclient/node";
-import { NSDebugger } from "./ns_db";
+import path from "path";
 
 let client: LanguageClient;
 
 function start_lsp_client(context: vscode.ExtensionContext) {
-  const server_module = context.asAbsolutePath("out/ns_lsp.js");
+  const server_module = context.asAbsolutePath("dist/ns_lsp.js");
   const server_options: ServerOptions = {
     run: { module: server_module, transport: TransportKind.ipc },
     debug: { module: server_module, transport: TransportKind.ipc },
@@ -28,13 +28,43 @@ function start_lsp_client(context: vscode.ExtensionContext) {
   console.log("ns_lsp client started");
 }
 
+const NSDB_DEBUG_TYPE = "nsdb";
+
+const DEFAULT_CONFIG: vscode.DebugConfiguration = {
+  type: NSDB_DEBUG_TYPE,
+  request: "launch",
+  name: "Debug NS Program"
+};
+
+class NSDebugConfigurationProvider implements vscode.DebugConfigurationProvider {
+  resolveDebugConfiguration(folder: vscode.WorkspaceFolder | undefined, config: vscode.DebugConfiguration, token?: vscode.CancellationToken): vscode.ProviderResult<vscode.DebugConfiguration> {
+    if (!config.type && !config.request && !config.name) {
+      return DEFAULT_CONFIG;
+    }
+    return config;
+  }
+
+  resolveDebugConfigurationWithSubstitutedVariables(folder: vscode.WorkspaceFolder | undefined, debugConfiguration: vscode.DebugConfiguration, token?: vscode.CancellationToken): vscode.ProviderResult<vscode.DebugConfiguration> {
+    return Object.assign({}, DEFAULT_CONFIG, debugConfiguration);
+  }
+}
+
+class NSDebugAdapterDescriptorFactory implements vscode.DebugAdapterDescriptorFactory {
+  createDebugAdapterDescriptor(session: vscode.DebugSession, executable: vscode.DebugAdapterExecutable | undefined): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
+    if (executable == null) {
+      executable = new vscode.DebugAdapterExecutable('node', ['/Users/bytedance/os/ns/nslang/dist/ns_db.js'], {});
+    }
+    return executable;
+  }
+}
+
 function start_dap_client(context: vscode.ExtensionContext) {
   context.subscriptions.push(
-    vscode.debug.registerDebugAdapterDescriptorFactory("nsdb", {
-      createDebugAdapterDescriptor: (session) => {
-        return new vscode.DebugAdapterInlineImplementation(new NSDebugger());
-      },
-    })
+    vscode.debug.registerDebugConfigurationProvider(NSDB_DEBUG_TYPE, new NSDebugConfigurationProvider())
+  );
+
+  context.subscriptions.push(
+    vscode.debug.registerDebugAdapterDescriptorFactory(NSDB_DEBUG_TYPE, new NSDebugAdapterDescriptorFactory())
   );
 }
 
