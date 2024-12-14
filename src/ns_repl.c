4ee70d5658bb29535fadfc21df3f0514033e4a2b
lang <lilang8936@gmail.com>
2024-12-14 11:26:37 +0800

    more repl

--- src/ns_repl.c
+++ src/ns_repl.c
@@ -6,6 +6,9 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
+static ns_vm _ns_repl_vm = {0};
+static ns_ast_ctx _ns_repl_ctx = {0};
+
 ns_str ns_repl_read_line(char *prompt) {
     char *line = readline(prompt);
     if (!line) return ns_str_null;
@@ -19,7 +22,6 @@ void ns_repl_free_line(ns_str s) {
 }
 
 void ns_repl(ns_vm* vm) {
-    ns_ast_ctx ctx = {0};
     vm->repl = true;
 
     // read eval pri32loop
@@ -28,22 +30,42 @@ void ns_repl(ns_vm* vm) {
         ns_str line = ns_repl_read_line(ns_color_log "ns" ns_color_nil "> ");
         if (line.len == 0) continue;
         // if get exit command
-        if (ns_str_equals_STR(line, "exit")) {
+        if (ns_str_equals_STR(line, "q") || ns_str_equals_STR(line, "exit")) {
             ns_repl_free_line(line);
             break;
         }
 
-        ns_return_bool ret_p = ns_ast_parse(&ctx, line, filename);
+        ns_return_bool ret_p = ns_ast_parse(&_ns_repl_ctx, line, filename);
         if (ns_return_is_error(ret_p)) {
             ns_warn("ast", "parse error: %.*s\n", ret_p.e.msg.len, ret_p.e.msg.data);
         }
 
-        ret_p = ns_vm_parse(vm, &ctx);
+        ret_p = ns_vm_parse(vm, &_ns_repl_ctx);
         if (ns_return_is_error(ret_p)) {
             ns_warn("parse", "vm parse error: %.*s\n", ret_p.e.msg.len, ret_p.e.msg.data);
         }
 
+        for (i32 i = _ns_repl_ctx.section_begin, l = _ns_repl_ctx.section_end; i < l; ++i) {
+            i32 s_i = _ns_repl_ctx.sections[i];
+            ns_ast_t *n = &_ns_repl_ctx.nodes[s_i];
+            ns_return_value ret = ns_return_ok(value, ns_nil);
+            if (n->type >= NS_AST_EXPR && n->type <= NS_AST_ARRAY_EXPR) {
+                ret = ns_eval_expr(vm, &_ns_repl_ctx, s_i);
+            } else if (n->type == NS_AST_VAR_DEF) {
+                ret = ns_eval_var_def(vm, &_ns_repl_ctx, s_i);
+            } else {
+                ns_warn("eval", "invalid expr type: %d\n", n->type);
+                continue;
+            }
+            if (ns_return_is_error(ret)) {
+                ns_warn("eval", "eval error: %.*s\n", ret.e.msg.len, ret.e.msg.data);
+            }
+            ns_str s = ns_fmt_value(vm, ret.r);
+            printf("%.*s\n", s.len, s.data);
+        }
         ns_repl_free_line(line);
+
+        _ns_repl_ctx.section_begin = _ns_repl_ctx.section_end;
     }
     ne_exit_safe("ns", "exit repl\n");
 }
